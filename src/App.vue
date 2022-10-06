c8ebd1c8f048b36c564024320df3ba8dbcfa643b
lisa <lis.lisaeva@gmail.com>
2022-10-06 00:08:12 -0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,59 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+
+    <div class="wrapper">
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/profile-details">Profile Details</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <div>
+
+  <RouterView />
+
+  </div>
+</template>
+
+<style scoped>
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+
+
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
+}
+</style>
---
+++ src/views/ProfileDetails.vue
@@ -0,0 +1,91 @@
+<template>
+    <div class="profileDetails">
+        <div class="wrapper">
+            <b-card
+                no-body
+                img-src="https://picsum.photos/600/300/?image=25"
+                img-alt="Image"
+                img-top
+                tag="article"
+                style="max-width: 30rem;"
+                class="mb-2">
+
+                <b-card-body>
+                    <b-card-title>Profile details: </b-card-title>
+                    <b-card-text>
+                        <table>
+                            <tr>
+                                <td><b>First name: </b></td>
+                                <td><b-form-input id="firstName" v-model="firstName" placeholder="Enter your first name"></b-form-input></td>
+                            </tr>
+
+                            <tr>
+                                <td><b>Last name: </b></td>
+                                <td><b-form-input id="lastName" v-model="lastName" placeholder="Enter your last name"></b-form-input></td>
+                            </tr>
+
+                            <tr>
+                                <td><b>Email: </b></td>
+                                <td><b-form-input id="email" v-model="email" placeholder="Enter your email address" type="email"></b-form-input></td>
+                            </tr>
+
+                        </table>
+                        <br>
+
+                        <b-button>View Saved Items</b-button>
+                    </b-card-text>
+                </b-card-body>
+
+                <hr>
+
+                <b-card-body>
+                    <b-card-text>
+                        <b-card-title>Change password: </b-card-title>
+
+                        <table>
+                            <tr>
+                                <td><b>Old password: </b></td>
+                                <td><b-form-input id="firstName" v-model="oldPassword" placeholder="Enter your current password"></b-form-input></td>
+                            </tr>
+
+                            <tr>
+                                <td><b>New password: </b></td>
+                                <td><b-form-input id="lastName" v-model="newPassword" placeholder="Enter your new password"></b-form-input></td>
+                            </tr>
+                        </table>
+
+                    </b-card-text>
+                </b-card-body>
+            </b-card>
+        </div>
+    </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        firstName: '',
+        lastName: '',
+        email: '',
+        oldPassword: '',
+        newPassword: ''
+      }
+    }
+  }
+
+</script>
+
+<style>
+@media (min-width: 1024px) {
+  .profileDetails {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+
+td {
+    padding: 5px 10px 0px 10px
+}
+</style>
