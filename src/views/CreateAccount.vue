f07bbbd298b1cc79fc407b703d62bd49b84043ab
aalnobel <60149151+aalnobel@users.noreply.github.com>
2022-10-20 17:20:13 -0500

    changed Create account page

--- src/views/CreateAccount.vue
+++ src/views/CreateAccount.vue
@@ -1,68 +1,90 @@
 <template>
-    <div class="create-account">
+    <div class="profileDetails">
         <div class="wrapper">
             <b-card no-body img-src="https://picsum.photos/600/300/?image=25" img-alt="Image" img-top tag="article"
-                style="max-width: 30rem;" class="mb-2">
+                style="max-width: 60rem;" class="mx-auto">
 
-                <b-card-body>
-                    <b-card-title>Profile details: </b-card-title>
-                    <b-card-text>
-                        <table>
-                            <tr>
-                                <td><b>First name: </b></td>
-                                <td>
-                                    <b-form-input id="firstName" v-model="firstName"
-                                        placeholder="Enter your first name"></b-form-input>
-                                </td>
-                            </tr>
+                <b-container>
+                    <b-row>
+                        <b-col cols="12" lg="6">
+                            <b-card-body>
+                                <b-card-title>Profile details</b-card-title>
+                                <b-container>
+                                    <b-row>
+                                        <b-col>
+                                            <!-- First name input field ------->
+                                            <b-form-group label-cols="4" content-cols="8" label="First name:"
+                                                label-for="first-name">
+                                                <b-form-input id="first-name" v-model="firstName"
+                                                    placeholder="Enter your first name"></b-form-input>
+                                            </b-form-group>
+                                        </b-col>
+                                    </b-row>
+                                    <b-row>
+                                        <b-col>
+                                            <!-- Last name input field -------->
+                                            <b-form-group label-cols="4" content-cols="8" label="Last name:"
+                                                label-for="last-name">
+                                                <b-form-input id="last-name" v-model="lastName"
+                                                    placeholder="Enter your last name">
+                                                </b-form-input>
+                                            </b-form-group>
+                                        </b-col>
+                                    </b-row>
 
-                            <tr>
-                                <td><b>Last name: </b></td>
-                                <td>
-                                    <b-form-input id="lastName" v-model="lastName" placeholder="Enter your last name">
-                                    </b-form-input>
-                                </td>
-                            </tr>
+                                    <b-row>
+                                        <b-col>
+                                            <!-- Email input field ------------>
+                                            <b-form-group label-cols="4" content-cols="8" label="Email:"
+                                                label-for="email">
+                                                <b-form-input id="email" v-model="email"
+                                                    placeholder="Enter your email address">
+                                                </b-form-input>
+                                            </b-form-group>
+                                        </b-col>
+                                    </b-row>
 
-                            <tr>
-                                <td><b>Email: </b></td>
-                                <td>
-                                    <b-form-input id="email" v-model="email" placeholder="Enter your email address"
-                                        type="email"></b-form-input>
-                                </td>
-                            </tr>
+                                    <b-row>
+                                        <b-col>
+                                            <!-- Password input field ------------>
+                                            <b-form-group label-cols="4" content-cols="8" label="Password:"
+                                                label-for="password">
+                                                <b-form-input id="password" v-model="password"
+                                                    placeholder="Enter your password">
+                                                </b-form-input>
+                                            </b-form-group>
+                                        </b-col>
+                                    </b-row>
 
-                            <tr>
-                                <td><b>Creat Password: </b></td>
-                                <td>
-                                    <b-form-input id="Password" v-model="email" placeholder="Enter your new Password"
-                                        type="password"></b-form-input>
-                                </td>
-                            </tr>
+                                    <b-row>
+                                        <b-col>
+                                            <!-- re-Password input field ------------>
+                                            <b-form-group label-cols="4" content-cols="8" label="Re-enter Password:"
+                                                label-for="ReEnterPassword">
+                                                <b-form-input id="ReEnterPassword" v-model="ReEnterPassword"
+                                                    placeholder="Enter your Password again">
+                                                </b-form-input>
+                                            </b-form-group>
+                                        </b-col>
+                                    </b-row>
 
-                            <tr>
-                                <td><b>Re-enter Password: </b></td>
-                                <td>
-                                    <b-form-input id="RePassword" v-model="email"
-                                        placeholder="Enter your Password again" type="confirmpassword"></b-form-input>
-                                </td>
-                            </tr>
-
-                        </table>
-                        <br>
-
-                        <b-button>Continue</b-button>
-                    </b-card-text>
-                </b-card-body>
-
-                <hr>
+                                </b-container>
+                            </b-card-body>
+                        </b-col>
 
 
+                        <b-col>
+                            <hr>
+                            <b-card-body>
+                                <b-button variant="success">Continue</b-button>
+                            </b-card-body>
+                        </b-col>
+                    </b-row>
+                </b-container>
             </b-card>
         </div>
     </div>
 </template>
-
 <script>
 export default {
     data() {
@@ -71,23 +93,8 @@ export default {
             lastName: '',
             email: '',
             Password: '',
-            RePassword: ''
+            ReEnterPassword: ''
         }
     }
 }
-
 </script>
-
-<style>
-@media (min-width: 1024px) {
-    .profileDetails {
-        min-height: 100vh;
-        display: flex;
-        align-items: center;
-    }
-}
-
-td {
-    padding: 5px 10px 0px 10px
-}
-</style>
