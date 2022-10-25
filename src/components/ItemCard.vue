a632476a231ba210aec5ec0a18637dc548e715bf
John Poppe <jdpoppe@uwm.edu>
2022-10-24 20:47:18 -0500

    Here's api reference stuff to play around

--- src/components/ItemCard.vue
+++ src/components/ItemCard.vue
@@ -1,17 +1,18 @@
 <template>
-    <b-card img-src="https://st4.depositphotos.com/14953852/24787/v/600/depositphotos_247872612-stock-illustration-no-image-available-icon-vector.jpg" img-alt="Product Image" img-top>
+    <div>
+    <b-card v-bind:img-src=img img-alt="Product Image" img-top>
         <b-card-body> 
             <b-link href="/">
-                <b-card-title><slot name="name"> Unknown </slot></b-card-title>
+                <b-card-title>{{name}}</b-card-title>
             </b-link>
                 <div style="display: flex; justify-content: space-between; ">
                     <p class="card-text">
                         <strong>Price:</strong> 
-                        <slot name="price"> $--.--</slot>
+                         <div> ${{price}}</div>
                     </p>
                     <p class="card-text">
-                        <strong>Vendor:</strong> 
-                        <slot name="Supplier"> Unknown </slot>
+                        <strong>Vendor: </strong> 
+                        <div>{{vendor}}</div> 
                     </p>
                 </div>
                     
@@ -21,19 +22,25 @@
                     </p>
 
                     <div>
-                        <fa icon="fa-solid fa-star"/>
-                        <fa icon="fa-solid fa-star"/>
-                        <fa icon="fa-solid fa-star"/>
-                        <fa icon="fa-solid fa-star"/>
-                        <fa icon="fa-regular fa-star" />
+                        <label> {{rating}} </label>
+                        <fa icon="fa-regular fa-star"/>
                     </div>
                     
                     
                     
         </b-card-body>
     </b-card>
+    </div>
 </template>
 
 <script>
-
+    export default{
+        props:{
+            name: String,
+            img: String,
+            vendor: String,
+            price: Number,
+            rating: Number
+        }
+    }
 </script>
--- src/views/SavedItems.vue
+++ src/views/SavedItems.vue
@@ -3,6 +3,9 @@
 <template>
   <UpperBar></UpperBar>
   <div>
+    <b-button v-on:click="search">Button</b-button>
+  </div>
+  <div>
     <b-card
         header="Saved Items"
         header-text-variant="white"
@@ -11,39 +14,68 @@
         footer-border-variant="dark"
     >
     <b-card-body>
-        <b-card-group deck>
-            <ItemCard></ItemCard>
-            <ItemCard></ItemCard>
-            <ItemCard></ItemCard>
+        <b-card-group deck v-for="item of items" style="display:flex">
+          <ItemCard 
+            v-bind:name=item.product.title 
+            v-bind:rating=item.product.rating
+            vendor="Target"
+            v-bind:img=item.product.main_image
+            v-bind:price=item.offers.primary.price
+          />
+            
         </b-card-group>
     </b-card-body>
   </b-card>
   </div>
+  <div>
+    <ul>
+      <li v-for="item of items" :key="item.id">
+        <img v-bind:src=item.product.main_image >     
+      </li>
+    </ul>
+  </div>
+  <div id="app">
+    <h1>Shopping List</h1>
+  </div>
+  <ul>
+    <li v-for="item of items" :key="item.id">
+      {{ item.product.title }}
+    </li>
+  </ul>
 </template>
 
 <script>
   import ItemCard from '../components/ItemCard.vue'
   import UpperBar from '../components/UpperBar.vue'
+  import axios from "axios";
   
   export default {
     components:{
         ItemCard,
         UpperBar
+    }, data() {
+      return {
+        categories: [],
+        facets: [],
+        items: []
+
+      }
+    },  methods: {
+      search: async function (event) {
+        try {
+        const res = await axios.get('https://api.redcircleapi.com/request?api_key=demo&type=search&search_term=highlighter+pens&sort_by=best_seller');
+        this.items = res.data.search_results;
+        this.categories = res.data.categories;
+        this.facets = data.facets;
+      } catch (error) {
+        console.log(error);
+      }
+      }
     }
   }
 
 </script>
 
 <style>
-@media (min-width: 1024px) {
-  .profileDetails {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
 
-td {
-    padding: 5px 10px 0px 10px
-}
 </style>
